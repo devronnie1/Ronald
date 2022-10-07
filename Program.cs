d8920541bf5024d9ea0f2154cc17465b628d05d4
unknown <oyinloyeronald@gmail.com>
2022-10-07 02:36:50 -0700

    Ronald

---
+++ Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+
+namespace ass_4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+               //question 1
+            // Console.WriteLine("Enter First Number");
+            // int FirstNumber = int.Parse(Console.ReadLine());
+            // Console.WriteLine("Enter Second Number");
+            // int SecondNumber = int.Parse(Console.ReadLine());
+            // Console.WriteLine("Enter Third Number");
+            // int ThirdNumber = int.Parse(Console.ReadLine());
+            // int sum = FirstNumber + SecondNumber + ThirdNumber;
+            // Console.WriteLine($"your sum is:  {sum}");
+
+               //question 2
+            //    Console.WriteLine("Calculate the perimeter  and area of circle");
+            //    Console.WriteLine("Enter your radius");
+            //    double radius = Convert.ToDouble(Console.ReadLine());
+            //    double r = radius * radius;
+            //    double d = 2 * radius;
+            //    double perimeter = Math.PI*d;
+            //    double area = Math.PI*r;
+            //    Console.WriteLine($"your permiter is: {perimeter} ");
+            //    Console.WriteLine($"your area is: {area} ");
+
+                //question 3
+                // Console.WriteLine("INFORMATION ABOUT COMPANY");
+                // Console.WriteLine("Enter your company name");
+                // string CompanyName = Console.ReadLine();
+                // Console.WriteLine("Enter your Company Add");
+                // string CompanyAdd = Console.ReadLine();
+                // Console.WriteLine("Enter your Phonenum");
+                // string PhoneNumb = Console.ReadLine();
+                // Console.WriteLine("Enter your fax number");
+                // string FaxNumber = Console.ReadLine();
+                // Console.WriteLine("Enter your Web site");
+                // string WebSite = Console.ReadLine();
+                // Console.WriteLine("Enter managers name");
+                // string ManagerName = Console.ReadLine();
+                // Console.WriteLine("Enter manager  Surname");
+                // string ManagerSurname = Console.ReadLine();
+                // Console.WriteLine("Enter managers phone number");
+                // string PhoneNumber = Console.ReadLine();
+                // Console.WriteLine($"My company Name is: {CompanyName} at {CompanyAdd} our company contact is {PhoneNumb},\n fax number {FaxNumber}, our company website is  {WebSite},\n and our manager name is {ManagerName}, his surname is  {ManagerSurname}, his contact is {PhoneNumber},\n do well to come for us");
+
+                // question 4
+                // long num1 = 22121212;
+                // long num2 = 3131313;
+                // long num3 = 44141414;
+                // Console.WriteLine("{0, -7}" ,  "|0x{0, -9:X}|", num1);
+                // Console.WriteLine("{0, -7]" ,  "|{0,-10:f2}|",num2);
+                // Console.WriteLine("0, -6)"  , num3);
+                 
+                 //question 6
+                //  Console.WriteLine("Enter first number");
+                //  double FirstNumber = double.Parse(Console.ReadLine());
+                //  Console.WriteLine("Enter Second Number");
+                //  double SecondNumber = double.Parse(Console.ReadLine());
+                //  double checkNumbers = Math.Max(FirstNumber, SecondNumber);
+                //  Console.WriteLine("Number {0} is greater", checkNumbers);
+
+                //question 7
+                // Console.WriteLine("Enter sum of First Number");
+                // long sumR = Convert.ToInt64(Console.ReadLine());
+                // Console.WriteLine("Enter aum of Second Number");
+                // long sumO = Convert.ToInt64(Console.ReadLine());
+                // Console.WriteLine("Enter sum of Third Number");
+                // long sumN = Convert.ToInt64(Console.ReadLine());
+                // Console.WriteLine("Enter sum of Forth Number");
+                // long sumA = Convert.ToInt64(Console.ReadLine());  
+                // Console.WriteLine("Enter sum of Fifth Number");
+                // long sumL = Convert.ToInt64(Console.ReadLine());
+
+                // long total = sumR + sumO + sumN + sumA + sumL;
+                // Console.WriteLine(total);
+                
+                //question 8
+                // Console.WriteLine("Enter first experiment");
+                // decimal sumA = Convert.ToDecimal(Console.ReadLine());
+                // Console.WriteLine("Enter second experiment");
+                // decimal sumB = Convert.ToDecimal(Console.ReadLine());
+                // Console.WriteLine("Enter THIRD experiment");
+                // decimal sumC = Convert.ToDecimal(Console.ReadLine());
+                // Console.WriteLine("Enter forth experiment");
+                // decimal sumdD= Convert.ToDecimal(Console.ReadLine());
+                // Console.WriteLine("Enter fifth experiment");
+                // decimal sumE = Convert.ToDecimal(Console.ReadLine());
+                // decimal sumF = Math.Max(sumA, sumB);
+                // decimal sumG = Math.Max(sumC, sumdD);
+                // decimal sumH = Math.Max(sumF, sumG);
+                // decimal sumI = Math.Max(sumH, sumE);
+                // Console.WriteLine(sumI);
+
+                //question 9
+                // Console.WriteLine("Enter first number");
+                // long n = Convert.ToInt64(Console.ReadLine());
+                // Console.WriteLine("Enter Second Number");
+                // long n1 = Convert.ToInt64(Console.ReadLine());
+                // long n2 = n + n1;
+                // Console.WriteLine(n2);
+
+                  //question 10
+                //   Console.WriteLine("Enter max number");
+                //   long Num1 = Convert.ToInt64(Console.ReadLine());
+                //   int i = 0;
+                  
+                //   Console.WriteLine("Enter a number");
+                //   int R = int.Parse(Console.ReadLine());
+
+                //   Console.WriteLine("Enter a number");
+                //   int O = int.Parse(Console.ReadLine());
+
+                //   if(R > O)
+                //   {
+                //       int N = O;
+                      
+                //   }
+
+                  
+
+                // //chapter (question 1)
+                // // int FirstNumber = 10;
+                // // int SecondNumber = 5;
+                // // if (FirstNumber > SecondNumber)
+                // // {
+                // //     int ThirdNumber = 0;
+                // //     FirstNumber = ThirdNumber;
+                // //     SecondNumber = FirstNumber;
+                // //     Console.WriteLine(FirstNumber);
+                // // }
+                // // if (SecondNumber == FirstNumber)
+                // // {
+                // //     int ForthNumber = 10;
+                // //     FirstNumber = 0;
+                // //     FirstNumber = (FirstNumber + ForthNumber);
+                // //     Console.WriteLine(FirstNumber);
+                // // }
+
+                // //question 3
+                // Console.WriteLine("Enter your FirstNumber");
+                // int FirstNumber = Convert.ToInt32(Console.ReadLine());
+                // Console.WriteLine("Enter your SeondNumber");
+                // int SecondNumber = Convert.ToInt32(Console.ReadLine());
+                // Console.WriteLine("Enter your ThirdNumber");     
+                // int ThirdNumber = Convert.ToInt32(Console.ReadLine());
+
+                // int biggerNumber = 0;
+                // if(FirstNumber > biggerNumber)
+                // {
+                //     biggerNumber = FirstNumber;
+                // }
+                // if(SecondNumber > biggerNumber);
+                // {
+                //     biggerNumber = SecondNumber;
+                // }
+                // if (ThirdNumber > biggerNumber)
+                // {
+                //     biggerNumber = ThirdNumber;
+                // }
+                // Console.WriteLine(biggerNumber);
+                
+                
+
+                
+                  
+                  
+                    
+                  
+                  
+                  
+                      
+                  
+                
+                        
+
+
+
+
+
+        }
+    }
+}
